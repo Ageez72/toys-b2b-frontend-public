87a716894ef7b0763d3c9a7723cd3d5f1803a021
mohamed.sobhy <mohamed.sobhy@linkdev.com>
2025-07-17 10:34:41 +0300

    hotfix

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -14,7 +14,7 @@ import { useQuery } from "@tanstack/react-query";
 
 // fallback images
 import fallbackDesktopImage from "@/assets/imgs/hero-bg.png";
-import fallbackMobileImage from "@/assets/imgs/hero.png";
+import fallbackMobileImage from "@/assets/imgs/hero-bg.png";
 
 export default function Home() {
   const { state = {}, dispatch = () => {} } = useAppContext() || {};
