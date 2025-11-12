3b10057a90ccc910a98577c0ba9bb399ec5a61dc
mohamed.sobhy <mohamed.sobhy@linkdev.com>
2025-11-12 10:46:40 +0200

    fix auto scroll

--- app/products/[productId]/page.jsx
+++ app/products/[productId]/page.jsx
@@ -54,9 +54,17 @@ export default function Page() {
     retry: false,
   });
 
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+  useEffect(() => {
+    scrollToTop()
+  }, []);
+
   // Effect to re-call the API whenever refresh is changed
   useEffect(() => {
     if (refresh) {
+      scrollToTop()
       refetch();
       setRefresh(false); // reset after fetching
     }
