c8b27d8d3cb622fd23ca37436e6c554bed1968e9
mohamed.sobhy <mohamed.sobhy@linkdev.com>
2025-12-18 15:47:48 +0200

    test

--- src/components/ui/Menu.jsx
+++ src/components/ui/Menu.jsx
@@ -65,7 +65,7 @@ export default function Menu({ scroll }) {
             <Link href="/brands" className="block py-2 px-3">{translation.brands}</Link>
           </li>
           {
-            cookiesState.clearance &&
+            cookiesState.clearance === "true" &&
             <li className="clearanceTab">
               <Link href="/products?itemType=CLEARANCE&itemStatus=AVAILABLE" className="block py-2 px-3">
                 {translation.clearance}
